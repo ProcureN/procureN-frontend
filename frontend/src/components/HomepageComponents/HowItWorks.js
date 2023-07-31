59d9d8655aba5999ffa14749fbaa53539a9065ce
salman170 <salmanansari02120@gmail.com>
2023-07-31 12:29:01 +0530

    duration added

--- frontend/src/components/HomepageComponents/HowItWorks.js
+++ frontend/src/components/HomepageComponents/HowItWorks.js
@@ -71,12 +71,8 @@ const HowItWorks = () => {
           <div className='title-font mb-3 text-center text-2xl font-medium  text-[#cb67ac] sm:text-3xl'>
             Vendor
           </div>
-          {/* <div className='mr-28 w-min whitespace-nowrap rounded-3xl border border-white bg-[#cb67ac] from-[#5c67f5] to-[#cb67ac] px-6 py-8 transition-all duration-100 hover:z-10 hover:scale-110 hover:bg-gradient-to-tr'>
-            Select your <br />
-            product
-          </div> */}
-          <div className='sm:mr-48 flex justify-start w-full  sm:justify-center hover:z-10 scale-95 sm:scale-100 hover:scale-100 sm:hover:scale-110'>
-            <div className='mr-2 flex h-full items-center text-end font-sans text-3xl md:text-5xl text-[#cb67ac] '>
+          <div className='flex w-full scale-95 justify-start  duration-500 hover:z-10 hover:scale-100 sm:mr-48 sm:scale-100 sm:justify-center sm:hover:scale-110 '>
+            <div className='mr-2 flex h-full items-center text-end font-sans text-3xl text-[#cb67ac] md:text-5xl '>
               01
             </div>
             <div className='top-10   w-min whitespace-nowrap rounded-3xl  border border-white bg-[#cb67ac] from-[#5c67f5] to-[#cb67ac] p-8 transition-all duration-100 hover:bg-gradient-to-tr '>
@@ -85,17 +81,17 @@ const HowItWorks = () => {
             </div>
           </div>
 
-          <div className='-mt-8 sm:ml-40 flex w-full justify-end scale-95 hover:scale-100 hover:z-10 sm:scale-100  sm:hover:scale-110 sm:justify-center'>
+          <div className='-mt-8 flex w-full scale-95 justify-end duration-500 hover:z-10 hover:scale-100 sm:ml-40  sm:scale-100 sm:justify-center sm:hover:scale-110  '>
             <div className='top-10   w-min whitespace-nowrap rounded-3xl  border border-white bg-[#5c67f5] from-[#5c67f5] to-[#cb67ac] p-8 transition-all duration-100 hover:bg-gradient-to-tr '>
               Proposal discussions <br />& verification
             </div>
-            <div className='ml-2 flex h-full items-center text-end font-sans  text-3xl md:text-5xl text-[#5c67f5] '>
+            <div className='ml-2 flex h-full items-center text-end font-sans  text-3xl text-[#5c67f5] md:text-5xl '>
               02
             </div>
           </div>
 
-          <div className='-mt-4 sm:mr-48 flex w-full scale-95 hover:scale-100 justify-start sm:scale-100  hover:z-10  sm:justify-center sm:hover:scale-110'>
-            <div className='mr-2 flex h-full items-center text-end font-sans text-3xl md:text-5xl text-[#cb67ac] '>
+          <div className='-mt-4 flex w-full scale-95 justify-start duration-500 hover:z-10 hover:scale-100  sm:mr-48  sm:scale-100 sm:justify-center sm:hover:scale-110  '>
+            <div className='mr-2 flex h-full items-center text-end font-sans text-3xl text-[#cb67ac] md:text-5xl '>
               03
             </div>
             <div className='   w-min whitespace-nowrap rounded-3xl  border border-white bg-[#cb67ac] from-[#5c67f5] to-[#cb67ac] p-8 transition-all duration-100 hover:bg-gradient-to-tr '>
@@ -103,11 +99,12 @@ const HowItWorks = () => {
               about new deals
             </div>
           </div>
-          <div className='-mt-8 flex w-full justify-end scale-95 hover:scale-100 sm:scale-100 hover:z-10 sm:hover:scale-110 sm:ml-40 sm:justify-center'>
+
+          <div className='-mt-8 flex w-full scale-95 justify-end duration-500 hover:z-10 hover:scale-100 sm:ml-40 sm:scale-100 sm:justify-center sm:hover:scale-110  '>
             <div className='top-10   w-min whitespace-nowrap rounded-3xl  border border-white bg-[#5c67f5] from-[#5c67f5] to-[#cb67ac] px-8 py-12 transition-all duration-100 hover:bg-gradient-to-tr '>
               Finalize the deal
             </div>
-            <div className='ml-2 flex h-full items-center text-end font-sans text-3xl md:text-5xl text-[#5c67f5] '>
+            <div className='ml-2 flex h-full items-center text-end font-sans text-3xl text-[#5c67f5] md:text-5xl '>
               04
             </div>
           </div>
@@ -122,12 +119,9 @@ const HowItWorks = () => {
           <div className='title-font mb-3 text-center text-2xl font-medium  text-[#cb67ac] sm:text-3xl'>
             Client
           </div>
-          {/* <div className='mr-28 w-min whitespace-nowrap rounded-3xl border border-white bg-[#cb67ac] from-[#5c67f5] to-[#cb67ac] px-6 py-8 transition-all duration-100 hover:z-10 hover:scale-110 hover:bg-gradient-to-tr'>
-            Select your <br />
-            product
-          </div> */}
-          <div className='flex  w-full justify-start scale-95 hover:scale-100 sm:scale-100  hover:z-10 sm:hover:scale-110 sm:mr-48 sm:justify-center'>
-            <div className='mr-2 flex h-full items-center text-end font-sans text-3xl md:text-5xl text-[#5c67f5] '>
+
+          <div className='flex  w-full scale-95 justify-start duration-500 hover:z-10  hover:scale-100 sm:mr-48 sm:scale-100 sm:justify-center  sm:hover:scale-110 '>
+            <div className='mr-2 flex h-full items-center text-end font-sans text-3xl text-[#5c67f5] md:text-5xl '>
               01
             </div>
             <div className='top-10   w-min whitespace-nowrap rounded-full  border border-white bg-[#5c67f5] from-[#5c67f5] to-[#cb67ac] p-8 transition-all duration-100 hover:bg-gradient-to-tr '>
@@ -135,25 +129,19 @@ const HowItWorks = () => {
               product
             </div>
           </div>
-          {/* <div className='top-20 -mt-4 ml-28 w-min whitespace-nowrap rounded-2xl  border border-white bg-[#cb67ac] px-4 py-6 transition-all duration-100 hover:z-10 hover:scale-110 '>
-            Send us an <br />
-            enquiry of product
-          </div> */}
-          <div className='-mt-8 flex w-full justify-end scale-95 hover:scale-100 hover:z-10 sm:scale-100  sm:hover:scale-110 sm:ml-40 sm:justify-center'>
+
+          <div className='-mt-8 flex w-full scale-95 justify-end duration-500 hover:z-10 hover:scale-100  sm:ml-40 sm:scale-100 sm:justify-center  sm:hover:scale-110 '>
             <div className='top-10   w-min whitespace-nowrap rounded-full  border border-white bg-[#cb67ac] from-[#5c67f5] to-[#cb67ac] p-8 transition-all duration-100 hover:bg-gradient-to-tr '>
               Send us an <br />
               enquiry of product
             </div>
-            <div className='ml-2 flex h-full items-center text-end font-sans text-3xl md:text-5xl text-[#cb67ac] '>
+            <div className='ml-2 flex h-full items-center text-end font-sans text-3xl text-[#cb67ac] md:text-5xl '>
               02
             </div>
           </div>
-          {/* <div className='top-40 -mt-4 mr-28 w-min whitespace-nowrap rounded-2xl  border border-white bg-[#cb67ac] from-[#5c67f5] to-[#cb67ac] px-6 py-10 transition-all duration-100 hover:z-10 hover:scale-110 hover:bg-gradient-to-tr '>
-            Receive response <br />
-            to your enquiry
-          </div> */}
-          <div className='-mt-6 flex w-full scale-95 hover:scale-100 justify-start sm:scale-100  hover:z-10 sm:mr-48 sm:justify-center sm:hover:scale-110'>
-            <div className='mr-2 flex h-full items-center text-end font-sans text-3xl md:text-5xl text-[#5c67f5] '>
+
+          <div className='-mt-6 flex w-full scale-95 justify-start duration-500 hover:z-10  hover:scale-100 sm:mr-48 sm:scale-100 sm:justify-center  sm:hover:scale-110 '>
+            <div className='mr-2 flex h-full items-center text-end font-sans text-3xl text-[#5c67f5] md:text-5xl '>
               03
             </div>
             <div className='   w-min whitespace-nowrap rounded-full   border border-white bg-[#5c67f5] from-[#5c67f5] to-[#cb67ac] p-8 transition-all duration-100 hover:bg-gradient-to-tr '>
@@ -161,22 +149,17 @@ const HowItWorks = () => {
               to your enquiry
             </div>
           </div>
-          <div className='-mt-6 flex w-full justify-end scale-95 hover:scale-100 sm:scale-100 hover:z-10 sm:hover:scale-110 sm:ml-40 sm:justify-center'>
+
+          <div className='-mt-6 flex w-full scale-95 justify-end duration-500 hover:z-10 hover:scale-100 sm:ml-40 sm:scale-100 sm:justify-center  sm:hover:scale-110 '>
             <div className='top-10   w-min whitespace-nowrap rounded-full   border border-white bg-[#cb67ac] from-[#5c67f5] to-[#cb67ac] px-8 py-10 transition-all duration-100 hover:bg-gradient-to-tr '>
               Finalize the deal
             </div>
-            <div className='ml-2 flex h-full items-center text-end font-sans text-3xl md:text-5xl text-[#cb67ac] '>
+            <div className='ml-2 flex h-full items-center text-end font-sans text-3xl text-[#cb67ac] md:text-5xl '>
               04
             </div>
           </div>
         </div>
       </div>
-      {/* <div className='text-white flex relative h-[60vh] '>
-        <div className='bg-[#5c67f5] rounded-full px-4 py-6 w-min whitespace-nowrap absolute left-[25%] hover:z-10 transition-all duration-500 border-2 border-white hover:scale-105 '>Send your product <br />or service details</div>
-        <div className='bg-[#cb67ac] rounded-2xl py-6 px-4 w-min whitespace-nowrap absolute right-1/2 top-20 hover:z-10 transition-all duration-500 border border-white hover:scale-105 '>Proposal discussions <br />& verification</div>
-        <div className='bg-[#5c67f5] rounded-3xl py-10 px-6 w-min whitespace-nowrap absolute left-[25%] top-40 hover:z-10 transition-all duration-500 border border-white hover:scale-105 '>Get informed <br />about new deals</div>
-        <div className='bg-[#cb67ac] rounded-2xl py-14 px-6 w-min whitespace-nowrap absolute right-1/2 top-60 hover:z-10 transition-all duration-500 border border-white hover:scale-105 '>Finalize the deal</div>
-      </div> */}
     </>
   );
 };
